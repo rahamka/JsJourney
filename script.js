b901fcd83f314445ba5610d1af07473ff5d2bd8f
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-12-28 12:49:31 +0500

    I understand how to use the setTimeout() in JavaScript.

--- script.js
+++ script.js
@@ -1,58 +1,20 @@
-// All Scopes in JavaScript.
-
-// Lexical Scope in JavaScript.
-
-function parentFun() {
-  let username = "Ahmad";
-  function add() {
-    console.log(5 + 3);
-  }
-  function childFun() {
-    console.log(username); // closure
-    add();
-  }
-  childFun();
+function a(b) {
+  // here a is Higher Order Function.
+  console.dir(b);
+  b();
 }
 
-parentFun();
-
-// Global Scope in JavaScript.
-
-let num1 = 55;
-const num2 = 56; // => const, let are string in the script scope
-var num3 = 57; // => var is storing in the Global scope.
-
-console.log(num1, num2, num3);
-
-function printFun() {
-  console.log(num1);
-}
-
-printFun();
-
-// Local Scope in JavaScript.
-function localFun() {
-  var ifVariable = false; // => false = 0
-  console.log(ifVariable + 1); // 1
-}
-
-localFun();
-
-// Objects in JavaScript.
-
-console.log("Objects in JavaScript.");
-
-let studentData = {
-  studentName: "Shoaib",
-  studentRoll: 112,
-  Class: "XII",
-  DOB: "12-05-2010",
+const sayHi = function () {
+  console.log(5 + 6);
+  console.log("Hiiiiiiiiii");
+  console.log("This is Anonymous Function.");
 };
 
-console.log(studentData["DOB"]);
+// here sayHi is callBack function because it is passed as an callBack function.
+a(sayHi);
 
-Object.seal(studentData);
-
-studentData.Class = "XI";
-
-console.log(studentData.Class);
+let value1 = setTimeout('console.log("Hello - 1")', 1000);
+clearTimeout(value1);
+console.log(value1);
+let value2 = setTimeout('console.log("Hello - 2")', 2000);
+let value3 = setTimeout('console.log("Hello - 3")', 3000);
